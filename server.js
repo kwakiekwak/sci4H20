35851441f371073a059b24602f4923adc9a8e850
John Byonghun Kwak <johnkwak1991@yahoo.com>
2016-03-07 02:55:18 -0800

    Creating Basic Routes

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ var express       = require('express'), // call express
     mongoose      = require('mongoose'), // for working w/ our databases MongoDB
     port          = process.env.PORT || 8080; // set the port for our app
 
+// require the User models
+var User = require('./models/user');
+
+// connect to our local database and name it sci4H20
+mongoose.connect('mongodb://localhost:27017/sci4H20');
+
 // APP CONFIGURATION
 // user body parser so we can grab information from POST requests
 app.use(bodyParser.urlencoded({extended: true }));
@@ -31,12 +37,25 @@ app.use(morgan('dev'));
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // basic route for the home page
+// checking to see if everything works
 app.get('/', function(req, res) {
   res.send('Welcome to the home page!');
 });
 
 // get an instance of the express router
-var apiRouter = express.Router();
+var apiRouter = express.Router(); // get an instance of the express Router
+
+// middeware to user for all requests
+apiRouter.use(function(req, res, next) {
+  // do loggin
+  console.log('Somebody just came to our app!');
+
+  // we'll add more to the middleware in Chapter 10
+  // this is where we will authenticate users
+
+
+  next(); // make sure we go to the next routes and don't stop here
+})
 
 // test route to make sure everything is working
 // accessed at GET http://localhost:8080/api
@@ -45,6 +64,7 @@ apiRouter.get('/', function(req, res) {
 });
 
 // more routes for our API will happen here
+
 
 // REGISTER OUR ROUTES
 // all of our routes will be prefixed with /api
